611cb6cdfa89d8a0b6f62b06040b2a9f15555eeb
Mr-spShuvo <mr.shahriyarshuvo@gmail.com>
2019-09-29 17:13:08 +0600

    services, whyus, testimonials

--- assets/js/aris-script.js
+++ assets/js/aris-script.js
@@ -1,1 +1,21 @@
 feather.replace();
+
+/*=============================
+ * Smooth Scrolling Function
+ *=============================*/
+
+function smoothScroller() {
+    var scrollLink = $('.scroll');
+
+    // Smooth scrolling
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').delay(100).animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1500);
+    });
+
+}
+
+smoothScroller();
+
