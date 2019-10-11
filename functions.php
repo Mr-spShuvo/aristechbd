0aae41d9cd40569ca4364c149bf94be973805c3b
Mr-spShuvo <mr.shahriyarshuvo@gmail.com>
2019-10-12 01:54:20 +0600

    final push v-1.0.1

--- functions.php
+++ functions.php
@@ -1,21 +1,16 @@
 <?php
 function aris_enqueue_scripts() {
-
     //Enqueuing Stylesheet
     wp_enqueue_style('wp-style', get_stylesheet_uri(), NULL, '1.0.1', 'all');
     wp_enqueue_style('aris-fonts', '//fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap" rel="stylesheet', NULL, '1.0.1', 'all');
     wp_enqueue_style('aris-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', NULL, '1.0.1', 'all');
     wp_enqueue_style('aris-style', get_template_directory_uri() . '/assets/css/aris-style.css', array('aris-fonts', 'aris-bootstrap'), '1.0.1', 'all');
 
-
     //Enqueuing Scripts
     wp_enqueue_script('aris-feather-icon', '//cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js', NULL, '1.0.1', true);
     wp_enqueue_script('aris-popperjs', get_template_directory_uri() . '/assets/js/popper.min.js', NULL, '1.0.1', true);
     wp_enqueue_script('aris-bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', NULL, '1.0.1', true);
-    wp_enqueue_script('aris-smoothjs', get_template_directory_uri() . '/assets/js/smooth.min.js', NULL, '1.0.1', true);
     wp_enqueue_script('aris-js', get_template_directory_uri() . '/assets/js/aris-scripts.js', array('jquery', 'aris-bootstrap'), '1.0.1', true);
-
-    ///
 }
 add_action('wp_enqueue_scripts', 'aris_enqueue_scripts');
 
